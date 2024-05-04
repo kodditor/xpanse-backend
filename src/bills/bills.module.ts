e543d4bf5649b5a0e6cb4c9830358f7ac4525797
gene-999 <eugenebaidoo5@gmail.com>
2024-05-04 02:37:14 +0000

    Bills component

--- src/bills/bills.module.ts
+++ src/bills/bills.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BillsService } from './bills.service';
 import { BillsController } from './bills.controller';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
-  providers: [BillsService],
+  providers: [BillsService, PrismaService],
   controllers: [BillsController]
 })
 export class BillsModule {}
--- src/bills/bills.service.ts
+++ src/bills/bills.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class BillsService {}
+export class BillsService {
+  constructor(private readonly db: PrismaService) {}
+
+  async getBills() {
+    return this.db.bill.findMany();
+  }
+
+  async findBill(where: Prisma.BillWhereUniqueInput) {
+    return this.db.bill.findUnique({ where });
+  }
+
+  async createBill(data: Prisma.BillCreateInput) {
+    return this.db.bill.create({ data });
+  }
+
+  async updateBill(
+    where: Prisma.BillWhereUniqueInput,
+    data: Prisma.BillUpdateInput,
+  ) {
+    return this.db.bill.update({
+      where,
+      data,
+    });
+  }
+
+  async deleteBill(where: Prisma.BillWhereUniqueInput) {
+    return this.db.bill.delete({
+      where,
+    });
+  }
+}
